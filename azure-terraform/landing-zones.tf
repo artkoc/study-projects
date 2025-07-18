2d16659de4dcc46f14c187a49961390265ab2d42
Artiom Kocharov <artiom.kocharov@gmail.com>
2025-07-18 15:19:21 +0200

    Update Terraform config

--- azure-terraform/landing-zones.tf
+++ azure-terraform/landing-zones.tf
@@ -23,3 +23,35 @@ resource "azurerm_log_analytics_workspace" "platform" {
   sku                 = "PerGB2018"  # Pricing tier for Log Analytics workspace
   retention_in_days   = 30  # Retains log data for 30 days
 }
+
+# VNET
+resource "azurerm_network_security_group" "nsgs" {
+  for_each= local.config.landing_zones
+  name     = format("NSG-%s-%s-%s",upper(local.config.global.location),local.config.landing_zones[each.key].id, local.config.landing_zones[each.key].sequence_no)
+  location = local.config.global.location  # 🟢 Uses region from 🔵 config.yaml → global.location
+  resource_group_name = azurerm_resource_group.rgs[each.key].name
+}
+
+/*
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name             = "subnet1"
+    address_prefixes = ["10.0.1.0/24"]
+  }
+
+  subnet {
+    name             = "subnet2"
+    address_prefixes = ["10.0.2.0/24"]
+    security_group   = azurerm_network_security_group.example.id
+  }
+
+  tags = {
+    environment = "Production"
+  }
+*/
